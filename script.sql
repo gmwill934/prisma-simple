4d04ea215ffe950bdfcddd7fe684ea174b662231
wmgsh <wm@goodsidehealth.com>
2022-08-22 16:19:53 -0500

    updated sql script

--- script.sql
+++ script.sql
@@ -1,11 +1,3 @@
-
-
-
-create table if not exists shipment (
-	id int,
-	data varchar(64)
-);
-
 create or replace function my_trigger() returns trigger as $$
 	begin
 		new.id = (select count(*) + 1 from shipment);
@@ -18,5 +10,5 @@ before insert on shipment
 for each row
 execute procedure my_trigger();
 
-insert into shipment (data) values ('my data');
+
 
